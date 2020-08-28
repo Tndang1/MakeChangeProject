3e4c619ce99f4626987272c2f1ad63f209f394c8
Thomas Dang <Thomas.N.Dang@gmail.com>
2020-08-28 16:57:41 -0500

    first commit

---
+++ src/makeChange/MakeChange.java
@@ -0,0 +1,23 @@
+package makeChange;
+
+import java.util.Scanner;
+
+public class MakeChange {
+
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Hello, welcome to Dang mart!");
+		System.out.println("Please enter your item cost!");
+		System.out.println(15 % 20);
+		double cost = kb.nextDouble();
+		System.out.println("Please enter how much money you are paying with! (x.xx)");
+		double payment = kb.nextDouble();
+	}
+	
+	public static double twenty(double cost, double payment) {
+		
+		
+		return remainder;
+	}
+
+}
