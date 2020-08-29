bc69555a7c05c1613bafce8748fa03f839dbab70
Thomas Dang <Thomas.N.Dang@gmail.com>
2020-08-29 18:39:32 -0500

    updated readme, added switch

--- src/makeChange/MakeChange.java
+++ src/makeChange/MakeChange.java
@@ -7,34 +7,77 @@ public class MakeChange {
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
 		System.out.println("Hello, welcome to Dang mart!");
-		System.out.println("Please enter your item cost! (xx.xx)");
+		System.out.print("Please enter your item cost! (xx.xx): ");
 		double cost = kb.nextDouble();
-		System.out.println("Please enter how much money you are paying with! (xx.xx)");
+		System.out.print("Please enter how much money you are paying with! (xx.xx): ");
 		double payment = kb.nextDouble();
 		if (cost == payment) {
 			System.out.println("Exact change? Well thank you!");
 		} else if (cost > payment) {
 			System.out.println("It's not enough! I even let you say how much it cost and how much you'd pay!");
+		} else if (cost < 0 || payment < 0){
+			System.out.println("I'm sorry, we aren't into negativity here.");
+			payment = 0;
+			cost = 0;
 		} else {
-			System.out.print("Amount: $" + cost + ", Tendered: $" + payment + " Result: ");
+			System.out.print("Amount: " + cost + ", Tendered: " + payment + " Result: ");
 		}
-
-		int due = (int) ((payment - cost) * 100);
-		while (due > 0) {
-
-			due = counter(
-					counter(counter(counter(counter(counter(counter(counter(due, 2000), 1000), 500), 100), 25), 10), 5),
-					1);
+		
+		int due = ((int) ((payment * 100) - (cost * 100)));
+		
+		for(int i = 0; i < 8; i++) {
+			int currency = 0;
+			switch(i) {
+			case 0:
+				currency = 2000;
+				break;
+			case 1:
+				currency = 1000;
+				break;
+			case 2:
+				currency = 500;
+				break;
+			case 3:
+				currency = 100;
+				break;
+			case 4:
+				currency = 25;
+				break;
+			case 5:
+				currency = 10;
+				break;
+			case 6:
+				currency = 5;
+				break;
+			case 7:
+				currency = 1;
+			}
+			due = counter(due, currency);
 		}
 		kb.close();
 	}
 
 	public static int counter(int due, int currency) {
+		String bill = "";
+		switch(currency) {
+		case 2000:
+			bill = "twenty";
+			break;
+		case 1000:
+			bill = "ten";
+			break;
+		case 500:
+			bill = "five";
+			break;
+		case 100:
+			bill = "one";
+			break;
+		}
 
 		int count = due / currency;
 		if (count > 0) {
 			if (currency >= 100) {
-				System.out.print(count + " " + currency / 100 + " dollar bill");
+				System.out.print(count + " " + bill + " dollar bill");
 				plural(count, due, currency);
 			} else {
 				switch (currency) {
@@ -73,8 +116,9 @@ public class MakeChange {
 		if (due % currency > 0) {
 			System.out.print(", ");
 		} else {
-			System.out.print(".");
+			System.out.println(".");
 		}
 
 	}
+
 }
